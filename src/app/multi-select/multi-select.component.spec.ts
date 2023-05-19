c5a7b1d731d8686d343c9006453f1304152dfecc
ismartDeveloper <101018479+ismartDeveloper@users.noreply.github.com>
2023-05-19 14:13:15 -0400

    updated

--- src/app/multi-select/multi-select.component.spec.ts
+++ src/app/multi-select/multi-select.component.spec.ts
@@ -19,42 +19,4 @@ describe('MultiSelectComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  describe('ComponentName', () => {
-    let component: ComponentName;
-  
-    beforeEach(() => {
-      component = new ComponentName();
-    });
-  
-    it('should open the popup if selecteddivision is defined and selecteddivisions length is less than or equal to 6', () => {
-      spyOn(window, 'alert');
-      component.selecteddivision = 'division';
-      component.selecteddivisions = ['division', 'division2', 'division3'];
-  
-      component.openPopup();
-  
-      expect(window.alert).toHaveBeenCalledWith('Popup opened!');
-    });
-  
-    it('should not open the popup if selecteddivision is not defined', () => {
-      spyOn(window, 'alert');
-      component.selecteddivision = null;
-      component.selecteddivisions = ['division', 'division2', 'division3'];
-  
-      component.openPopup();
-  
-      expect(window.alert).not.toHaveBeenCalled();
-    });
-  
-    it('should not open the popup if selecteddivisions length is greater than 6', () => {
-      spyOn(window, 'alert');
-      component.selecteddivision = 'division';
-      component.selecteddivisions = ['division', 'division2', 'division3', 'division4', 'division5', 'division6', 'division7'];
-  
-      component.openPopup();
-  
-      expect(window.alert).not.toHaveBeenCalled();
-    });
-  });
-  
 });
