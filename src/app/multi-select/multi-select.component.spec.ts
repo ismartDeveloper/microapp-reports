bab9da3e7462c049195b83b3791574121ad6c1d0
ismartDeveloper <101018479+ismartDeveloper@users.noreply.github.com>
2023-05-19 14:04:53 -0400

    updated

--- src/app/multi-select/multi-select.component.spec.ts
+++ src/app/multi-select/multi-select.component.spec.ts
@@ -26,30 +26,30 @@ describe('MultiSelectComponent', () => {
       component = new ComponentName();
     });
   
-    it('should open the popup if selectedRegion is defined and selectedRegions length is less than or equal to 6', () => {
+    it('should open the popup if selecteddivision is defined and selecteddivisions length is less than or equal to 6', () => {
       spyOn(window, 'alert');
-      component.selectedRegion = 'region1';
-      component.selectedRegions = ['region1', 'region2', 'region3'];
+      component.selecteddivision = 'division';
+      component.selecteddivisions = ['division', 'division2', 'division3'];
   
       component.openPopup();
   
       expect(window.alert).toHaveBeenCalledWith('Popup opened!');
     });
   
-    it('should not open the popup if selectedRegion is not defined', () => {
+    it('should not open the popup if selecteddivision is not defined', () => {
       spyOn(window, 'alert');
-      component.selectedRegion = null;
-      component.selectedRegions = ['region1', 'region2', 'region3'];
+      component.selecteddivision = null;
+      component.selecteddivisions = ['division', 'division2', 'division3'];
   
       component.openPopup();
   
       expect(window.alert).not.toHaveBeenCalled();
     });
   
-    it('should not open the popup if selectedRegions length is greater than 6', () => {
+    it('should not open the popup if selecteddivisions length is greater than 6', () => {
       spyOn(window, 'alert');
-      component.selectedRegion = 'region1';
-      component.selectedRegions = ['region1', 'region2', 'region3', 'region4', 'region5', 'region6', 'region7'];
+      component.selecteddivision = 'division';
+      component.selecteddivisions = ['division', 'division2', 'division3', 'division4', 'division5', 'division6', 'division7'];
   
       component.openPopup();
   
