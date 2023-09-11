1cf7a238541f558fb20aead7adad20559ea83745
ismartDeveloper <101018479+ismartDeveloper@users.noreply.github.com>
2023-09-11 14:03:51 -0400

    added validate

--- src/app/validate/validate.component.ts
+++ src/app/validate/validate.component.ts
@@ -7,28 +7,12 @@ import { Component } from '@angular/core';
 })
 export class ValidateComponent {
   sections = [
-    {
-      labelA: 'Label 1 A',
-      valueA: 'Value 1 A',
-      labelB: 'Label 1 B',
-      valueB: 'Value 1 B',
-      labelC: 'Label 1 C',
-      valueC: 'Value 1 C',
-      labelD: 'Label 1 D',
-      valueD: 'Value 1 D',
-    },
-    {
-      labelA: 'Label 2 A',
-      valueA: 'Value 2 A',
-      labelB: 'Label 2 B',
-      valueB: 'Value 2 B',
-      labelC: 'Label 2 C',
-      valueC: 'Value 2 C',
-      labelD: 'Label 2 D',
-      valueD: 'Value 2 D',
-    }
-    
+    { label: 'Label A', value: 'Value A' },
+    { label: 'Label B', value: 'Value B' },
+    { label: 'Label C', value: 'Value C' },
+    { label: 'Label D', value: 'Value D' }
   ];
+    
   
   
 }
