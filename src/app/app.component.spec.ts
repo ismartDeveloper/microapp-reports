144c12ce16682abf0e9aa64e418ba170f3c2106f
ismartDeveloper <101018479+ismartDeveloper@users.noreply.github.com>
2023-09-13 22:38:39 -0400

    test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,30 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { BehaviorComponent } from './behavior.component';
-import { TestComponent } from '../test/test.component';
-import { YioooComponent } from '../yiooo/yiooo.component';
+import { ConversionNameRendererComponent } from './conversion-name-renderer.component';
 
-describe('BehaviorComponent', () => {
-  let fixture: ComponentFixture<BehaviorComponent>;
-  let component: BehaviorComponent;
+describe('ConversionNameRendererComponent', () => {
+  let component: ConversionNameRendererComponent;
+  let fixture: ComponentFixture<ConversionNameRendererComponent>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [BehaviorComponent, TestComponent, YioooComponent],
-    });
-    fixture = TestBed.createComponent(BehaviorComponent);
-    component = fixture.componentInstance;
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ConversionNameRendererComponent],
+    }).compileComponents();
   });
 
-  it('should create the BehaviorComponent', () => {
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ConversionNameRendererComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create the component', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should use <test> and <yiooo> components', () => {
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    const testComponent = compiled.querySelector('app-test');
-    const yioooComponent = compiled.querySelector('app-yiooo');
-    expect(testComponent).toBeTruthy();
-    expect(yioooComponent).toBeTruthy();
+  it('should emit a selected event when clicked and not disabled', () => {
+    const mockRowData = { conversionName: 'Sample Conversion', reportStatus: 'Success' };
+    component.rowData = mockRowData;
+    spyOn(component.selected, 'emit');
+    const linkElement = fixture.nativeElement.querySelector('sdf-link');
+    linkElement.click();
+    expect(component.selected.emit).toHaveBeenCalledWith(mockRowData);
+  });
+
+  it('should not emit a selected event when clicked and disabled', () => {
+    const mockRowData = { conversionName: 'Sample Conversion', reportStatus: 'Failure' };
+    component.rowData = mockRowData;
+    spyOn(component.selected, 'emit');
+    const linkElement = fixture.nativeElement.querySelector('sdf-link');
+    linkElement.click();
+    expect(component.selected.emit).not.toHaveBeenCalled();
   });
 });
